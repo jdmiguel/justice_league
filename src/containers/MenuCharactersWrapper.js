ccc435ee46f6746ccb23f4565764417ce9c7da26
jdmiguel <jaime.miguel@i3television.es>
2018-08-23 15:23:32 +0200

    background over letters animation added

--- src/containers/MenuCharactersWrapper.js
+++ src/containers/MenuCharactersWrapper.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Background from '../components/MenuCharacters/Background/Background';
 import Letters from '../components/MenuCharacters/LettersCharacter';
-import Logo from '../components/MenuCharacters/LogoCharacter';
+import LogoCharacter from '../components/MenuCharacters/LogoCharacter';
 
 const MenuCharactersWrapper = () => (
   <div className="menuCharacters_wrapper">
     <Background />
     <Letters />
-    <Logo />
+    <LogoCharacter />
   </div>
 );
 
--- src/containers/app.js
+++ src/containers/app.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import Wrapper from '../hoc/Wrapper';
 import Router from '../router';
+import Logo from '../components/Logo';
 
-const App = () => <Router />;
+const App = () => (
+  <Wrapper>
+    <Logo />
+    <Router />
+  </Wrapper>
+);
 
 export default hot(module)(App);
