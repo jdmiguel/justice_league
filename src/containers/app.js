090e877dba79e12fde083cfa7496fcded29796bb
jdmiguel <jaimedmiguel@gmail.com>
2018-09-16 23:44:14 +0200

    logo_justice_league created

--- src/containers/app.js
+++ src/containers/app.js
@@ -3,10 +3,12 @@ import { hot } from 'react-hot-loader';
 import Wrapper from '../hoc/Wrapper';
 import Router from '../router';
 import LogoDC from '../components/Logos/logoDC';
+import LogoJL from '../components/Logos/logoJL';
 
 const App = () => (
   <Wrapper>
     <LogoDC />
+    <LogoJL />
     <Router />
   </Wrapper>
 );
