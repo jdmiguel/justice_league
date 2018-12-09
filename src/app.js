3e5d0580a10015eee69035ac781b413f23e53bf9
jdmiguel <jaimedmiguel@gmail.com>
2018-12-09 16:01:26 +0100

    hoc wrapper removed

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import Wrapper from './hoc/Wrapper';
 import Router from './router';
 import LogoDC from './components/HeaderLogo/logoDC';
 import LogoJL from './components/HeaderLogo/logoJL';
 import Lansdcape from './components/Landscape';
 
 const App = () => (
-  <Wrapper>
+  <div className="app">
     <Lansdcape />
     <LogoDC />
     <LogoJL />
     <Router />
-  </Wrapper>
+  </div>
 );
 
 export default hot(module)(App);
