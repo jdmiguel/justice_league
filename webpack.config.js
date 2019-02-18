693f6bad783aea343fcedc8f6eab21cfd85f8e80
jdmiguel <jaimedmiguel@gmail.com>
2019-02-18 21:50:07 +0100

    img optimized

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,9 @@ module.exports = {
         use: [
           {
             loader: 'file-loader',
-            options: {}
+            options: {
+              name: '[name].[ext]'
+            }
           }
         ]
       }
