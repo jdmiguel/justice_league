bc8584ae634f40918c44027d027d25d7d0f403a8
jdmiguel <jaimedmiguel@gmail.com>
2019-08-13 15:29:05 +0200

    utils svgs ready, reducer created

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
-import Icon from '../core/icon';
-import { BatmanIcon } from '../../utils/svgs';
-
 export default () => (
   <footer>
-    <Icon svg={BatmanIcon} width="50px" height="50px" />
     <div className="created">
       <h3>
         Created by{' '}
