521bec903ace1fb05a0dee8e865ff5641086458d
jdmiguel <jaimedmiguel@gmail.com>
2019-08-26 22:25:59 +0200

    data sheet reducer implemented

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -36,11 +36,11 @@ export const initialState = {
         {
           section: 'Origin / connections',
           data: [
-            { title: 'Full name', text: 'Clark Joseph Kent' },
+            { title: 'Full name', text: 'Clark Kent' },
             { title: 'Place of birth', text: 'Krypton' },
             {
               title: 'Occupation',
-              text: 'Reporter / Novelist'
+              text: 'Reporter'
             },
             { title: 'Base', text: 'Metropolis' },
             { title: 'First appearance', text: 'Action Comics #1' }
@@ -69,10 +69,35 @@ export const initialState = {
     },
     {
       name: 'Batman',
-      alias: 'Bruce Wayne',
+      alias: 'The dark night',
       class: 'batman',
       intro:
-        'Bruce Wayne, who witnessed the murder of his billionaire parents as a child, swore to avenge their deaths. He trained extensively to achieve several things such as mental and physical perfection, mastering martial arts, detective skills, and criminal psychology. Costumed as a bat to prey on the fears of criminals, and utilizing a high-tech arsenal, he became the legendary dark night.',
+        'Bruce Wayne, who witnessed the murder of his billionaire parents as a child, swore to avenge their deaths. He trained extensively to achieve mental and physical perfection, mastering martial arts, detective skills, and criminal psychology. Costumed as a bat to prey on the fears of criminals, and utilizing a high-tech arsenal, he became the legendary batman.',
+      dataSheet: [
+        {
+          section: 'Origin / connections',
+          data: [
+            { title: 'Full name', text: 'Bruce Wayne' },
+            { title: 'Place of birth', text: 'Gotham City' },
+            {
+              title: 'Occupation',
+              text: 'Businessman'
+            },
+            { title: 'Base', text: 'Gotham City' },
+            { title: 'First appearance', text: 'Detective Comics #27' }
+          ]
+        },
+        {
+          section: 'Appearance',
+          data: [
+            { title: 'Race', text: 'Human' },
+            { title: 'Height', text: '6.2 / 188 cm' },
+            { title: 'Weight', text: '210 lb / 95 kg' },
+            { title: 'Eye color', text: 'Blue' },
+            { title: 'Hair color', text: 'Black' }
+          ]
+        }
+      ],
       active: false,
       breakpoint: 2,
       index: 1,
@@ -85,10 +110,35 @@ export const initialState = {
     },
     {
       name: 'WonderWoman',
-      alias: 'Diana Prince',
+      alias: 'Princess of the Amazons',
       intro:
-        'The princess of the Amazons, armed with superpowers of a god, Wonder Woman is one of Earth´s most powerful defenders of peace, justice, and equality and a member of the Justice League. Her original origin allegorically depicted her as a clay figure brought to life by the gods, but in recent years she has been depicted more literally as the daughter of Zeus and the Amazon queen Hippolyta.',
+        'Armed with superpowers of a god, Wonder Woman is one of Earth´s most powerful defenders of peace, justice, and equality and a member of the Justice League. Her original origin allegorically depicted her as a clay figure brought to life by the gods, but in recent years she has been depicted more literally as the daughter of Zeus and the Amazon queen Hippolyta.',
       class: 'wonderwoman',
+      dataSheet: [
+        {
+          section: 'Origin / connections',
+          data: [
+            { title: 'Full name', text: 'Diana Prince' },
+            { title: 'Place of birth', text: 'Themyscira' },
+            {
+              title: 'Occupation',
+              text: 'Adventurer'
+            },
+            { title: 'Base', text: 'Paradise Island' },
+            { title: 'First appearance', text: 'All-Star Comics #8' }
+          ]
+        },
+        {
+          section: 'Appearance',
+          data: [
+            { title: 'Race', text: 'Amazon' },
+            { title: 'Height', text: '6.0 / 183 cm' },
+            { title: 'Weight', text: '165 lb / 74 kg' },
+            { title: 'Eye color', text: 'Blue' },
+            { title: 'Hair color', text: 'Black' }
+          ]
+        }
+      ],
       active: false,
       breakpoint: 5,
       index: 2,
@@ -101,10 +151,35 @@ export const initialState = {
     },
     {
       name: 'TheFlash',
-      alias: 'Barry Allen',
+      alias: 'the Fastest Man Alive',
       intro:
-        'Having discovered his mother murdered and his father blamed for the act, forensic scientist Barry Allen sought to clear his father´s alias and find the real killer. After being doused in chemicals and struck by lightning, Barry was granted the gift of super-speed. Now he protects his hometown of Central City as The Flash, the fastest man alive and founding member of the Justice League.',
+        'Having discovered his mother murdered and his father blamed for the act, forensic scientist Barry Allen sought to clear his father´s alias and find the real killer. After being doused in chemicals and struck by lightning, Barry was granted the gift of super-speed. Nowadays he protects his hometown of Central City as The Flash, the scarlet speedster.',
       class: 'flash',
+      dataSheet: [
+        {
+          section: 'Origin / connections',
+          data: [
+            { title: 'Full name', text: '	Barry Allen' },
+            { title: 'Place of birth', text: 'Fallville, Iowa' },
+            {
+              title: 'Occupation',
+              text: 'Forensic Scientist'
+            },
+            { title: 'Base', text: 'Central City' },
+            { title: 'First appearance', text: 'Showcase #4' }
+          ]
+        },
+        {
+          section: 'Appearance',
+          data: [
+            { title: 'Race', text: 'Human' },
+            { title: 'Height', text: '6.0 / 183 cm' },
+            { title: 'Weight', text: '195 lb / 88 kg' },
+            { title: 'Eye color', text: 'Blue' },
+            { title: 'Hair color', text: 'Blond' }
+          ]
+        }
+      ],
       active: false,
       breakpoint: 3,
       index: 3,
@@ -117,10 +192,35 @@ export const initialState = {
     },
     {
       name: 'GreenLantern',
-      alias: 'Hal Jordan',
+      alias: 'God of Light',
       intro:
-        'With the ability to overcome great fear and harness the power of will, test-pilot Hal Jordan was chosen to be the Green Lantern of Sector 2814 inheriting the ring of the dying alien Green Lantern, Abin Sur. Through sheer will power and determination, Hal has established an impressive record of heroism across the galaxy with the help of his fellow Green Lanterns as well as his peers in the Justice League.',
+        'With the ability to overcome great fear and harness the power of will, test-pilot Hal Jordan was chosen to be the Green Lantern of Sector 2814 inheriting the ring of the dying alien Green Lantern, Abin Sur. Through sheer will power and determination, Hal has established an impressive record of heroism across the galaxy with the help of his fellow Green Lanterns.',
       class: 'greenlantern',
+      dataSheet: [
+        {
+          section: 'Origin / connections',
+          data: [
+            { title: 'Full name', text: 'Hal Jordan' },
+            { title: 'Place of birth', text: 'Coast City, California' },
+            {
+              title: 'Occupation',
+              text: 'Air Force Pilot'
+            },
+            { title: 'Base', text: 'Oa Planet' },
+            { title: 'First appearance', text: 'Showcase #22' }
+          ]
+        },
+        {
+          section: 'Appearance',
+          data: [
+            { title: 'Race', text: 'Human' },
+            { title: 'Height', text: '6.2 / 188 cm' },
+            { title: 'Weight', text: '200 lb / 90 kg' },
+            { title: 'Eye color', text: 'Brown' },
+            { title: 'Hair color', text: 'Brown' }
+          ]
+        }
+      ],
       active: false,
       breakpoint: 5,
       index: 4,
@@ -133,10 +233,35 @@ export const initialState = {
     },
     {
       name: 'Aquaman',
-      alias: 'Arthur Curry',
+      alias: 'The King of the Seven Seas',
       intro:
-        'The son of an Atlantean queen and a lighthouse keeper from the town of Amnesty Bay, Arthur Curry would grow up to become the superhero Aquaman, and later take on his birthright as the King of Atlantis. He is the bridge between the surface world and his tumultuous realm of the sea. Furthermore, he is a founding member of the Justice League and is among DC Comics´ most recognized heroes.',
+        'The son of an Atlantean queen and a lighthouse keeper from the town of Amnesty Bay, Arthur Curry would grow up to become the superhero Aquaman, and later take on his birthright as the King of Atlantis. He is the bridge between the surface world and his tumultuous realm of the sea, in addition to being a notable founding member of the Justice League.',
       class: 'aquaman',
+      dataSheet: [
+        {
+          section: 'Origin / connections',
+          data: [
+            { title: 'Full name', text: 'Arthur Curry' },
+            { title: 'Place of birth', text: 'Amnesty Bay, Massachusetts' },
+            {
+              title: 'Occupation',
+              text: 'Adventurer'
+            },
+            { title: 'Base', text: 'Atlantis' },
+            { title: 'First appearance', text: 'More Fun Comics #73' }
+          ]
+        },
+        {
+          section: 'Appearance',
+          data: [
+            { title: 'Race', text: 'Atlantean' },
+            { title: 'Height', text: '6.1 / 185 cm' },
+            { title: 'Weight', text: '325 lb / 146 kg' },
+            { title: 'Eye color', text: 'Blue' },
+            { title: 'Hair color', text: 'Blond' }
+          ]
+        }
+      ],
       active: false,
       breakpoint: 3,
       index: 5,
@@ -149,10 +274,35 @@ export const initialState = {
     },
     {
       name: 'GreenArrow',
-      alias: 'Oliver Queen',
+      alias: 'The hood',
       intro:
-        'Oliver Jonas "Ollie" Queen was a spoiled, thrill-seeking playboy, until he was left stranded on a deserted island for several years. There, he trained tirelessly to become a master archer and powerful fighter in order to survive in such wild and cruel scenario. After his return home, he used his new-found skills and his wealth to became the costumed vigilante known as Green Arrow.',
+        'Oliver Jonas "Ollie" Queen was a spoiled, thrill-seeking playboy, until he was left stranded on a deserted island for several years. There, he trained tirelessly to become a master archer and powerful fighter in order to survive. After his return home, he used his new-found skills and his wealth to became the costumed vigilante known as Green Arrow.',
       class: 'greenarrow',
+      dataSheet: [
+        {
+          section: 'Origin / connections',
+          data: [
+            { title: 'Full name', text: 'Oliver Queen' },
+            { title: 'Place of birth', text: 'Starling City' },
+            {
+              title: 'Occupation',
+              text: 'Businessman'
+            },
+            { title: 'Base', text: 'Star City' },
+            { title: 'First appearance', text: 'Arrow Season 1' }
+          ]
+        },
+        {
+          section: 'Appearance',
+          data: [
+            { title: 'Race', text: 'Human' },
+            { title: 'Height', text: '6.1 / 185 cm' },
+            { title: 'Weight', text: '182 lb / 82 kg' },
+            { title: 'Eye color', text: 'Blue' },
+            { title: 'Hair color', text: 'Brown' }
+          ]
+        }
+      ],
       active: false,
       breakpoint: 5,
       index: 6,
@@ -165,10 +315,35 @@ export const initialState = {
     },
     {
       name: 'Cyborg',
-      alias: 'Victor Stone',
+      alias: 'Cyberion Technis',
       intro:
-        'Half man, half machine - all hero! After a near fatal incident, Victor Stone was cybernetically enhanced by his father. He now possesses the ability to communicate, manipulate, and interface with nearly all forms of technology. As he is constantly upgrading, he promises to defend the future from any threat. He is also a founding member of both, the Justice League and the Teen Titans.',
+        'Half man, half machine - all hero! After a near fatal incident, Victor Stone was cybernetically enhanced by his father. He now possesses the ability to communicate, manipulate, and interface with nearly all forms of technology. As he is constantly upgrading, he promises to defend the future from any threat. He is also a founding member of the Justice League.',
       class: 'cyborg',
+      dataSheet: [
+        {
+          section: 'Origin / connections',
+          data: [
+            { title: 'Full name', text: 'Victor Stone' },
+            { title: 'Place of birth', text: 'New York City, New York' },
+            {
+              title: 'Occupation',
+              text: 'Adventurer'
+            },
+            { title: 'Base', text: 'San Francisco' },
+            { title: 'First appearance', text: 'DC Comics Presents #26' }
+          ]
+        },
+        {
+          section: 'Appearance',
+          data: [
+            { title: 'Race', text: 'Cyborg' },
+            { title: 'Height', text: '6.6 / 198 cm' },
+            { title: 'Weight', text: '385 lb / 173 kg' },
+            { title: 'Eye color', text: 'Brown' },
+            { title: 'Hair color', text: 'Black' }
+          ]
+        }
+      ],
       active: false,
       breakpoint: 2,
       index: 7,
