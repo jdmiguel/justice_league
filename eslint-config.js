f00ae29689b8e7851d0c2b4fdd98e6647c94facb
Jaime <jaimedmiguel@gmail.com>
2020-05-10 14:26:56 +0100

    eslint rules removed

--- eslint-config.js
+++ eslint-config.js
@@ -32,34 +32,4 @@ module.exports = {
       jsx: true,
     },
   },
-  rules: {
-    indent: 0, // eslint vs prettier. Round 1! Fight!
-    curly: ['error', 'all'],
-    'import/prefer-default-export': 'off',
-    'no-alert': 'error',
-    'no-debugger': 'error',
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'linebreak-style': 'off',
-    'lines-around-comment': ['error', { beforeBlockComment: true }],
-    'multiline-comment-style': ['error', 'separate-lines'],
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'prettier/prettier': [
-      'error',
-      {
-        // Number of spaces it should use per tab
-        tabWidth: 2,
-        singleQuote: true,
-
-        // Controls the printing of trailing commas wherever possible. Valid options:
-        // 'none' - No trailing commas
-        // 'es5'  - Trailing commas where valid in ES5 (objects, arrays, etc)
-        // 'all'  - Trailing commas wherever possible (function arguments)
-        trailingComma: 'none',
-
-        // If true, puts the `>` of a multi-line jsx element at the end of
-        // The last line instead of being alone on the next line
-        jsxBracketSameLine: false,
-      },
-    ],
-  },
 };
